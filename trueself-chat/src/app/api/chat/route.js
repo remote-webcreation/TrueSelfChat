76c66d191957c01df3be2a3b4110b2ee54f22008
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-08-19 17:05:04 +0200

    finished POST-handler function for requests from frontend

--- trueself-chat/src/app/api/chat/route.js
+++ trueself-chat/src/app/api/chat/route.js
@@ -4,3 +4,22 @@ import { NextResponse } from 'next/server';
 const openai = new OpenAi({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+export async function POST(req) {
+    try {
+        const { messages } = await req.json();
+
+        if (!messages) {
+            return NextResponse.json({ message: 'Messages are required.'}), { status: 400 };
+        }
+
+        const completion = await openai.chat.completions.create({
+            model: "gpt-4o",
+            messages: messages,
+        });
+
+        return NextResponse.json(completion.choices[0].message);
+    } catch (error) {
+        return NextResponse.json({ message: 'Something went wrong with the API call.' }, { status: 500 });
+    }
+}
